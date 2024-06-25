ca01e22a06039ca8b2f162a1a3dee84c732c1f46
abdulrahman <abdulrahmanazam58@gmail.com>
2024-06-26 01:30:00 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,32 @@
+// Assalamualaikum @everyone 👋
+
+// 🚀 Day 38 Challenge: Start Coding! 🚀
+
+// Question 112: Create a Map that stores the names of countries as keys and their capitals as values. Add three countries to the Map.
+const countries = new Map<string, string>();
+countries.set("Pakistan", "Islamabaad");
+countries.set("Japan", "Tokyo");
+countries.set("Afghanistan", "Kabul");
+console.log(countries);
+console.log(`\n`);
+
+// Question 113: Write a function that checks if a Map contains a key for "Canada" and logs the capital if it exists.
+function checkTheCanadaInMap(countries: Map<string, string>): void{
+    if(countries.has("Canada")){
+        console.log(`The capital of Canada is ${countries.get("Canada")}`);
+    } else {
+        console.log("Canada is not in the map");
+    }
+}
+checkTheCanadaInMap(countries);
+console.log(`\n`);
+
+// Question 114: Iterate over a Map of student IDs and names, and log each pair to the console.
+const students = new Map<number, string>();
+students.set(1, "Ali");
+students.set(2, "Zain");
+students.set(3, "Faiz");
+
+students.forEach((name , id) => {
+    console.log(`Student ID : ${id}, Name: ${name}`);
+});
